06561e489aa5de199ac1c91c07673e265eaea0df
Kacper_Wdowiak <kwdowiak@student.agh.edu.pl>
2025-01-06 21:48:08 +0100

    Refactor Grass class

--- project_darwin_world/src/main/java/project/model/worldelements/Grass.java
+++ project_darwin_world/src/main/java/project/model/worldelements/Grass.java
@@ -2,12 +2,14 @@ package project.model.worldelements;
 
 import project.model.movement.Vector2d;
 
-public class Grass implements WorldElement {
+public class Grass implements Plant {
     public final static String GRASS_STRING = "*";
     private final Vector2d position;
+    private final int nutritiousness;
 
-    public Grass(Vector2d position) {
+    public Grass(Vector2d position, int nutritiousness) {
         this.position = position;
+        this.nutritiousness = nutritiousness;
     }
 
     @Override
@@ -15,6 +17,10 @@ public class Grass implements WorldElement {
         return this.position;
     }
 
+    @Override
+    public int getEnergy() {
+        return nutritiousness;
+    }
     @Override
     public String toString() {
         return Grass.GRASS_STRING;
