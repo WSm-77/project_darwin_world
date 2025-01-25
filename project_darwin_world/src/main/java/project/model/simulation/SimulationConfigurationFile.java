2a6f712a1ad479b0b8c507e33981a5140142d801
Kacper_Wdowiak <kwdowiak@student.agh.edu.pl>
2025-01-25 17:00:13 +0100

    Add config pattern file

---
+++ project_darwin_world/src/main/java/project/model/simulation/SimulationConfigurationFile.java
@@ -0,0 +1,17 @@
+package project.model.simulation;
+
+public class SimulationConfigurationFile {
+    public double initialAnimalCount;
+    public double initialAnimalEnergy;
+    public double energyToReproduce;
+    public double initialChildEnergy;
+    public double genomeLength;
+    public double minMutations;
+    public double maxMutations;
+    public double initialPlantCount;
+    public double plantNutritiousness;
+    public double dailyPlantGrowth;
+    public double mapWidth;
+    public double mapHeight;
+    public boolean simulationRefreshType;
+}
