b1b11d9c9f93bdb77959670a733587bd9a8716db
Kacper_Wdowiak <kwdowiak@student.agh.edu.pl>
2025-01-27 22:39:38 +0100

    Export statistics methods to SimulationStatistics class

--- project_darwin_world/src/main/java/project/presenter/ChartPaneController.java
+++ project_darwin_world/src/main/java/project/presenter/ChartPaneController.java
@@ -4,17 +4,18 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.chart.*;
 import javafx.scene.control.Label;
-import project.model.map.WorldMap;
-import project.model.movement.Vector2d;
+import project.model.simulation.Simulation;
 import project.model.simulation.SimulationEvent;
-import project.model.worldelements.Animal;
-import project.model.worldelements.WorldElement;
+import project.model.simulation.SimulationListener;
+import project.model.simulation.SimulationStatistics;
 
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.Optional;
+import java.util.Objects;
 
-public class ChartPaneController extends AbstractController {
+public class ChartPaneController extends AbstractController implements SimulationListener {
+    public static final String SIMULATION_STATISTICS = "Simulation statistics";
+    public static final String VALUES = "Values";
     @FXML
     private Label mostPopularGenomeLabel;
     @FXML
@@ -36,71 +37,65 @@ public class ChartPaneController extends AbstractController {
     private XYChart.Series<String, Double> plantsCountSeries = new XYChart.Series<>();
     private XYChart.Series<String, Double> averageAnimalsEnergy = new XYChart.Series<>();
     private XYChart.Series<String, Double> averageAnimalsLifeTime = new XYChart.Series<>();
-    private XYChart.Series<String, Double> averageAnimalsChilderenCount = new XYChart.Series<>();
+    private XYChart.Series<String, Double> averageAnimalsChildrenCount = new XYChart.Series<>();
+
+    private Simulation simulation;
+    private SimulationStatistics statistics;
 
     @FXML
     public void initialize() {
         Axis<String> xAxis = this.simulationStatisticChart.getXAxis();
         Axis<Double> yAxis = this.simulationStatisticChart.getYAxis();
 
-        xAxis.setLabel("Simulation statistics");
-        yAxis.setLabel("Values");
+        xAxis.setLabel(SIMULATION_STATISTICS);
+        yAxis.setLabel(VALUES);
 
-        XYChart.Data<String, Double> animalsCountChartData = new XYChart.Data<>(ANIMALS_COUNT_SERIES_NAME, 0.0);
-        XYChart.Data<String, Double> plantsCountChartData = new XYChart.Data<>(PLANTS_COUNT_SERIES_NAME, 0.0);
-        XYChart.Data<String, Double> averageAnimalsEnergyChartData = new XYChart.Data<>(AVERAGE_ANIMAL_ENERGY_SERIES_NAME, 0.0);
-        XYChart.Data<String, Double> averageAnimalLifeTimeChartData = new XYChart.Data<>(AVERAGE_ANIMAL_LIFE_TIME_SERIES_NAME, 0.0);
-        XYChart.Data<String, Double> averageAnimalChildrenCountChartData = new XYChart.Data<>(AVERAGE_ANIMAL_CHILDREN_COUNT_SERIES_NAME, 0.0);
-
-        this.animalsCountSeries.getData().add(animalsCountChartData);
-        this.plantsCountSeries.getData().add(plantsCountChartData);
-        this.averageAnimalsEnergy.getData().add(averageAnimalsEnergyChartData);
-        this.averageAnimalsLifeTime.getData().add(averageAnimalLifeTimeChartData);
-        this.averageAnimalsChilderenCount.getData().add(averageAnimalChildrenCountChartData);
-
-        this.animalsCountSeries.setName(ANIMALS_COUNT_SERIES_NAME);
-        this.plantsCountSeries.setName(PLANTS_COUNT_SERIES_NAME);
-        this.averageAnimalsEnergy.setName(AVERAGE_ANIMAL_ENERGY_SERIES_NAME);
-        this.averageAnimalsLifeTime.setName(AVERAGE_ANIMAL_LIFE_TIME_SERIES_NAME);
-        this.averageAnimalsChilderenCount.setName(AVERAGE_ANIMAL_CHILDREN_COUNT_SERIES_NAME);
+        animalsCountSeries.setName(ANIMALS_COUNT_SERIES_NAME);
+        plantsCountSeries.setName(PLANTS_COUNT_SERIES_NAME);
+        averageAnimalsEnergy.setName(AVERAGE_ANIMAL_ENERGY_SERIES_NAME);
+        averageAnimalsLifeTime.setName(AVERAGE_ANIMAL_LIFE_TIME_SERIES_NAME);
+        averageAnimalsChildrenCount.setName(AVERAGE_ANIMAL_CHILDREN_COUNT_SERIES_NAME);
 
         this.simulationStatisticChart.getData().addAll(
                 animalsCountSeries,
                 plantsCountSeries,
                 averageAnimalsEnergy,
                 averageAnimalsLifeTime,
-                averageAnimalsChilderenCount
+                averageAnimalsChildrenCount
         );
     }
 
-    @Override
-    public void simulationChanged(SimulationEvent simulationEvent) {
-        switch (simulationEvent) {
-            case MAP_CHANGED -> Platform.runLater(this::updateMapInformation);
-            case NEXT_DAY -> Platform.runLater(this::updateDay);
-        }
+    public void setSimulation(Simulation simulation) {
+        this.simulation = simulation;
+        this.statistics = new SimulationStatistics(simulation);
+        simulation.subscribe(this);
+    }
 
-        Platform.runLater(this::updateEmptySpots);
+    @Override
+    public void simulationChanged(SimulationEvent event) {
+        if (event == SimulationEvent.NEXT_DAY) {
+            Platform.runLater(this::updateCharts);
+            Platform.runLater(this::updateEmptySpots);
+            Platform.runLater(this::updateMostPopularGenome);
+            Platform.runLater(this::updateDay);
+        }
+    }
+
+    private void updateCharts() {
+        this.updateSeries(animalsCountSeries, (double) statistics.getAnimalsCount());
+        this.updateSeries(plantsCountSeries, (double) statistics.getPlantsCount());
+        this.updateSeries(averageAnimalsEnergy, statistics.calculateAverageEnergy());
+        this.updateSeries(averageAnimalsLifeTime, statistics.calculateAverageLifeTime());
+        this.updateSeries(averageAnimalsChildrenCount, statistics.calculateAverageAnimalChildrenCount());
     }
 
     private void updateEmptySpots() {
-        String emptySpotsText = String.format(EMPTY_SPOTS_TEMPLATE, this.calculateNumberOfEmptySpots());
-        this.numberOfEmptySpotsLabel.setText(emptySpotsText);
-    }
-
-    private void updateDay() {
-        String simulationDay = String.format(SIMULATION_DAY_TEMPLATE, this.simulation.getDay());
-        this.simulationStatisticChart.setTitle(simulationDay);
-    }
-
-    private void updateMapInformation() {
-        this.updateCharts();
-        this.updateEmptySpots();
-        this.updateMostPopularGenome();
+        int emptySpots = statistics.calculateNumberOfEmptySpots();
+        this.numberOfEmptySpotsLabel.setText(String.format(EMPTY_SPOTS_TEMPLATE, emptySpots));
     }
 
     private void updateMostPopularGenome() {
-        Optional<List<Integer>> mostPopularOptionalGenesList = this.mostPopularGenome();
+        Optional<List<Integer>> mostPopularOptionalGenesList = statistics.mostPopularGenome();
         String genomeString = mostPopularOptionalGenesList
                 .map(Objects::toString)
                 .orElse(NO_INFORMATION_STRING);
@@ -109,91 +104,19 @@ public class ChartPaneController extends AbstractController {
         this.mostPopularGenomeLabel.setText(mostPopularGenomeText);
     }
 
-    private void updateCharts() {
-        this.updateSeries(this.animalsCountSeries, (double) this.getAnimalsCount());
-        this.updateSeries(this.plantsCountSeries, (double) this.getPlantsCount());
-        this.updateSeries(this.averageAnimalsEnergy, this.calculateAverageEnergy());
-        this.updateSeries(this.averageAnimalsLifeTime, this.calculateAverageLifeTime());
-        this.updateSeries(this.averageAnimalsChilderenCount, this.calculateAverageAnimalChildrenCount());
+    private void updateDay() {
+        String simulationDay = String.format(SIMULATION_DAY_TEMPLATE, this.simulation.getDay());
+        this.simulationStatisticChart.setTitle(simulationDay);
     }
 
     private void updateSeries(XYChart.Series<String, Double> series, Double value) {
-        var data = series.getData().getFirst();
+        XYChart.Data<String, Double> data = series.getData().isEmpty()
+                ? new XYChart.Data<>(series.getName(), value)
+                : series.getData().get(0);
+
         data.setYValue(value);
-    }
-
-    private int getAnimalsCount() {
-        var worldMap = this.simulation.getWorldMap();
-        var animalsList = worldMap.getAnimals();
-
-        return  animalsList.size();
-    }
-
-    private int getPlantsCount() {
-        var worldMap = this.simulation.getWorldMap();
-        var plantsList = worldMap.getPlants();
-
-        return plantsList.size();
-    }
-
-    private int calculateNumberOfEmptySpots() {
-        var worldMap = this.simulation.getWorldMap();
-
-        var plantsList = worldMap.getPlants();
-        var animalsList = worldMap.getAnimals();
-
-        Stream<Vector2d> plantsPositionsStream = plantsList.stream().map(WorldElement::getPosition);
-        Stream<Vector2d> animalsPositionsStream = animalsList.stream().map(WorldElement::getPosition);
-        Set<Vector2d> occupiedPositions = Stream.concat(plantsPositionsStream, animalsPositionsStream)
-                .collect(Collectors.toSet());
-
-        int totalSpots = worldMap.getWidth() * worldMap.getHeight();
-
-        return totalSpots - occupiedPositions.size();
-    }
-
-    private double calculateAverageEnergy() {
-        WorldMap worldMap = this.simulation.getWorldMap();
-        List<Animal> animals = worldMap.getAnimals();
-
-        return animals.stream()
-                .mapToDouble(animal -> animal.getStatistics().getEnergy())
-                .average()
-                .orElse(0.0);
-    }
-
-    private double calculateAverageLifeTime() {
-        List<Animal> deadAnimals = this.simulation.getDeadAnimals();
-
-        return deadAnimals.stream()
-                .mapToDouble(animal -> (double) animal.getStatistics().getDaysAlive())
-                .average()
-                .orElse(0.0);
-    }
-
-    private double calculateAverageAnimalChildrenCount() {
-        WorldMap worldMap = this.simulation.getWorldMap();
-        List<Animal> animals = worldMap.getAnimals();
-
-        return animals.stream()
-                .mapToDouble(animal -> animal.getStatistics().getChildrenCount())
-                .average()
-                .orElse(0.0);
-    }
-
-    private Optional<List<Integer>> mostPopularGenome() {
-        WorldMap worldMap = this.simulation.getWorldMap();
-        List<Animal> aliveAnimals = worldMap.getAnimals();
-
-        HashMap<List<Integer>, Integer> genomeCountMap = new HashMap<>();
-        for (var animal : aliveAnimals) {
-            List<Integer> genesList = animal.getStatistics().getGenesList();
-            Integer genomeCount = genomeCountMap.getOrDefault(genesList, 0) + 1;
-            genomeCountMap.put(genesList, genomeCount);
+        if (series.getData().isEmpty()) {
+            series.getData().add(data);
         }
-
-        return genomeCountMap.entrySet().stream()
-                .max(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey);
     }
 }
